68547d420fb7a4386aba68ad46c59682ed45362d
rodrigomsrocha <rodrigomtc82@gmail.com>
2022-04-25 19:13:56 -0300

    sign in funcionality

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,8 +1,23 @@
-import { AiOutlineGoogle } from "react-icons/ai";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { AiOutlineClose, AiOutlineGoogle } from "react-icons/ai";
 
 export function SignInButton() {
-  return (
-    <button className="justify-self-end flex gap-4 items-center bg-purple-600 px-4 py-2 rounded-md hover:bg-purple-700">
+  const { data: session } = useSession();
+
+  return session ? (
+    <button
+      onClick={() => signOut()}
+      className="justify-self-end flex gap-4 items-center bg-purple-600 px-4 py-2 rounded-md hover:bg-purple-700"
+    >
+      <AiOutlineGoogle size="24" />
+      {session.user.name}
+      <AiOutlineClose />
+    </button>
+  ) : (
+    <button
+      onClick={() => signIn()}
+      className="justify-self-end flex gap-4 items-center bg-purple-600 px-4 py-2 rounded-md hover:bg-purple-700"
+    >
       <AiOutlineGoogle size="24" />
       Sign in with Google
     </button>
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,33 @@
+import { query as q } from "faunadb";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { fauna } from "../../../services/fauna";
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async signIn({ user }) {
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email: user.email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
+          )
+        );
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
+});
